97f8548f0520b07067929e0e35815793da7da8f5
Pukkok <krystalgggg@gmail.com>
2025-04-02 16:12:48 +0900

    [Design] 호버시 디자인 수정

--- front/app/components/SignModal/ModalInput.js
+++ front/app/components/SignModal/ModalInput.js
@@ -1,24 +1,27 @@
 const ModalInput = ({type="text", placeholder="입력값 넣을 정보", ...props}) => {
 
   return (
-    <input 
-      type={type} placeholder={placeholder} {...props}
-      
-      className="
-      bg-gradient-to-r from-[#1f2124] to-[#27292c]
-      border border-[#222]
-      rounded-[.3em]
-      shadow-[0_1px_0_rgba(255,255,255,0.1)]
-      text-[#fff]
-      text-[13px]
-      mb-[20px]
-      px-2 py-2
-      w-full
-      
-      group-hover:bg-[#27292c]
-      "
-      
-    />
+    <p className="">
+      <input 
+        type={type} placeholder={placeholder} {...props}
+        
+        className="
+        bg-linear-to-b from-[#1f2124] to-[#27292c]
+        border border-[#222]
+        rounded-[.3em]
+        shadow-[0_1px_0_rgba(255,255,255,0.1)]
+        text-[#fff]
+        text-[13px]
+        mb-[20px]
+        px-2 py-2
+        w-full
+        
+        hover:bg-none
+        hover:bg-[#27292c]
+        "
+        
+      />
+    </p>
   )
 }
 
