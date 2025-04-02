6c6ae1072e708ca63132c0cc9c9351e33663a875
Pukkok <krystalgggg@gmail.com>
2025-04-02 15:40:46 +0900

    [Feat] 메인페이지 연결

--- front/app/page.js
+++ front/app/page.js
@@ -1,9 +1,9 @@
-import SignModal from "./components/SignModal"
+import MainPage from "./main/Mainpage"
 
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-500">
-      <SignModal />
+      <MainPage />
     </div>
   )
 }
