06c28171c3e7437718681f4a03a668469347f73d
Pukkok <krystalgggg@gmail.com>
2025-04-10 18:36:16 +0900

    [Feat] 로그인 버튼 컴포넌트 생성

---
+++ front/app/components/LoginButton.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+const LoginButton = ({ onClick }) => (
+  <button 
+    onClick={onClick}
+    className="text-sm bg-[#ffbb00] text-[#000] px-4 py-1 rounded-md hover:bg-[#e6a800] transition-all"
+  >
+    로그인
+  </button>
+)
+
+export default LoginButton
