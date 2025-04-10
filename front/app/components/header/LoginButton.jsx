3697f035591c272982e9a0b8fde2db34e32b0c15
Pukkok <krystalgggg@gmail.com>
2025-04-10 23:30:30 +0900

    [Design] 로그인 버튼 커서 포인터 추가

--- front/app/components/header/LoginButton.jsx
+++ front/app/components/header/LoginButton.jsx
@@ -3,7 +3,7 @@
 const LoginButton = ({ onClick }) => (
   <button 
     onClick={onClick}
-    className="text-sm bg-[#ffbb00] text-[#000] px-4 py-1 rounded-md hover:bg-[#e6a800] transition-all"
+    className="text-sm bg-[#ffbb00] text-[#000] px-4 py-1 rounded-md hover:bg-[#e6a800] transition-all cursor-pointer"
   >
     로그인
   </button>
