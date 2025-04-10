2cc1040756df094b4bfeb296765c90bc5f3a2407
Pukkok <krystalgggg@gmail.com>
2025-04-10 22:26:09 +0900

    [Feat] 플레이 아이콘 생성

---
+++ front/app/components/Icons/PlayIcon.jsx
@@ -0,0 +1,11 @@
+const PlayIcon = ({ className = "w-4 h-4 fill-current" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+  >
+    <path d="M5 3v18l15-9L5 3z" />
+  </svg>
+)
+
+export default PlayIcon
