9a2cb67bf2a5ad66930f9354157caa00385b9917
Pukkok <krystalgggg@gmail.com>
2025-04-10 23:30:15 +0900

    [Design] 헤더 고정으로 변경

--- front/app/components/header/Header.jsx
+++ front/app/components/header/Header.jsx
@@ -9,9 +9,8 @@ import Container from '../Container'
 const Header = ({ onLoginClick }) => {
 
   return (
-    <header>
-      <Container className='flex items-center gap-12
-    py-4 shadow-md'>
+    <header className='fixed top-0 z-10 left-0 right-0'>
+      <Container className='flex items-center gap-12 py-4'>
         <Logo />
         <NavMenu />
         <SearchBar />
