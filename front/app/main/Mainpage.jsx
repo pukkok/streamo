b7844d6e8ea3a3d818f696e7af359d96edda200c
Pukkok <krystalgggg@gmail.com>
2025-04-10 18:38:12 +0900

    [Feat] 헤더 - 로그인 모달 연결

---
+++ front/app/main/Mainpage.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Header from '../components/Header'
+import SignModal from '../components/SignModal' // ← 이거 연결!
+
+const MainPage = () => {
+  const [signInModalOpen, setSignInModalOpen] = useState(false)
+
+  const openSignModal = () => {
+    setSignInModalOpen(true)
+  }
+
+  const closeSignModal = () => {
+    setSignInModalOpen(false)
+  }
+
+  return (
+    <div className="">
+      <Header onLoginClick={openSignModal} />
+      
+      {signInModalOpen && (
+        <SignModal onClose={closeSignModal} />
+      )}
+
+      {/* 히어로 섹션 넣기 */}
+      {/* 카테고리 리스트 넣기 */}
+    </div>
+  )
+}
+
+export default MainPage
