256b656e713d45f37fcae0f56a78f0c721c5795a
Pukkok <krystalgggg@gmail.com>
2025-04-10 18:36:31 +0900

    [Feat] 로고 컴포넌트 생성

---
+++ front/app/components/Logo.jsx
@@ -0,0 +1,10 @@
+'use client'
+import Link from 'next/link'
+
+const Logo = () => (
+  <Link href="/" className="text-[#ffbb00] font-bold text-xl tracking-wide">
+    Streamo
+  </Link>
+)
+
+export default Logo
