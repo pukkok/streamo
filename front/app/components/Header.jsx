4e47bb320617c555a915032b0c476d820696cc03
Pukkok <krystalgggg@gmail.com>
2025-04-10 22:44:01 +0900

    [Feat] 헤더 Container 적용

--- front/app/components/Header.jsx
+++ front/app/components/Header.jsx
@@ -4,18 +4,21 @@ import Logo from './Logo'
 import NavMenu from './NavMenu'
 import SearchBar from './SearchBar'
 import LoginButton from './LoginButton'
+import Container from './Container'
 
 const Header = ({ onLoginClick }) => {
 
   return (
-    <header className="flex items-center gap-12
-    px-6 py-4 bg-[#111] shadow-md">
-      <Logo />
-      <NavMenu />
-      <SearchBar />
-      <div className="flex items-center gap-4 ml-auto">
-        <LoginButton onClick={onLoginClick} />
-      </div>
+    <header className="bg-[#111]">
+      <Container className='flex items-center gap-12
+    py-4 bg-[#111] shadow-md'>
+        <Logo />
+        <NavMenu />
+        <SearchBar />
+        <div className="flex items-center gap-4 ml-auto">
+          <LoginButton onClick={onLoginClick} />
+        </div>
+      </Container>
     </header>
   )
 }
