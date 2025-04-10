e1b2ae250c9a861941b9bac85dc05029f32f8efd
Pukkok <krystalgggg@gmail.com>
2025-04-10 23:31:30 +0900

    [Feat] 카테고리 섹션 연결

--- front/app/main/Mainpage.jsx
+++ front/app/main/Mainpage.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import Header from '../components/header/Header'
 import SignModal from '../components/signModal/SignModal' // ← 이거 연결!
 import HeroSection from '../components/hero/HeroSection'
+import CategorySection from '../components/category/CategorySection'
+import dummyCategory from '../components/category/dummyCategory'
+
 
 const MainPage = () => {
   const [signInModalOpen, setSignInModalOpen] = useState(false)
@@ -15,7 +18,7 @@ const MainPage = () => {
   }
 
   return (
-    <div className="">
+    <>
       <Header onLoginClick={openSignModal} />
       
       {signInModalOpen && (
@@ -25,7 +28,8 @@ const MainPage = () => {
       {/* 히어로 섹션 넣기 */}
       <HeroSection />
       {/* 카테고리 리스트 넣기 */}
-    </div>
+      <CategorySection title={dummyCategory.title} items={dummyCategory.items} />
+    </>
   )
 }
 
