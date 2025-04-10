3264a42100f8a5c786fb2f6e8e7685f171543ad5
Pukkok <krystalgggg@gmail.com>
2025-04-10 22:32:19 +0900

    [Feat] 히어로 섹션 연결

--- front/app/main/Mainpage.jsx
+++ front/app/main/Mainpage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from '../components/Header'
 import SignModal from '../components/SignModal' // ← 이거 연결!
+import HeroSection from '../components/hero/HeroSection'
 
 const MainPage = () => {
   const [signInModalOpen, setSignInModalOpen] = useState(false)
@@ -22,6 +23,7 @@ const MainPage = () => {
       )}
 
       {/* 히어로 섹션 넣기 */}
+      <HeroSection />
       {/* 카테고리 리스트 넣기 */}
     </div>
   )
