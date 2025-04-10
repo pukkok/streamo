a1e589b87a9d83280784eb5b3783c3a1ddf7c647
Pukkok <krystalgggg@gmail.com>
2025-04-10 23:26:02 +0900

    [Design] 다크모드 사용 설정

--- front/app/layout.js
+++ front/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`mgr-0 pd-0`}>
+      <body className={`mgr-0 pd-0 bg-[#111] text-white`}>
         {children}
       </body>
     </html>
